58824907bdb7d1726e3161b3c84909f8ff8e35b3
Vinicius Alves <viniciusalvesms757@gmail.com>
2024-05-14 14:56:33 -0300

    feature: full project completed

---
+++ src/TerminalAccount.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class TerminalAccount {
+    public static void main(String[] args) {
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner input = new Scanner(System.in);
+
+        //TODO: Exibir as mensagens para nosso usuário
+        Display("Por favor, digite o nome da agência: ");
+        String agency = input.next();
+
+        Display("Por favor, digite o número da Agência !");
+        int agencyID = input.nextInt();
+
+        Display("Digite seu nome: ");
+        String name = input.next();
+
+        Display("Digite sua idade: ");
+        int age = input.nextInt();
+        if(age < 18) {
+            Display("A criação de conta está disponível apenas para maiores de idade !");
+            return;
+        }
+
+        Display("Digite sua senha: ");
+        int password = input.nextInt();
+
+        Display("Confirme sua senha: ");
+        int confirmPassword = input.nextInt();
+
+        Display("Digite seu saldo: ");
+        double balance = input.nextDouble();
+
+        //TODO: Exibir mensagem de conta criada
+        if(confirmPassword == password) {
+            Display("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + agencyID + " e seu saldo " + "R$" + balance + " já está disponível para saque.");
+        } else {
+            Display("Senhas não coincidem");
+        }
+    }
+
+    public static void Display(String message){
+        System.out.println(message);
+    }
+
+}
